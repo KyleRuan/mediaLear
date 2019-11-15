c92ab6c606123687046addb9213b8b9a3fc3502d
ruankai <ruankai@kuaishou.com>
2019-11-15 15:01:24 +0800

    fix ff 打包成mac-os x64

--- 3day/build_fdk.sh
+++ 3day/build_fdk.sh
@@ -2,15 +2,15 @@
 
 FDK_ROOT_DIR="`pwd`"
 
-$FDK_ROOT_DIR/fdk-aac-sdk/configure \
+$FDK_ROOT_DIR/opencore-amr-fdk-aac/configure \
 --enable-static \
 --disable-shared \
---host=arm-apple-darwin \
+--host=aarch64-apple-darwin \
 --prefix="$FDK_ROOT_DIR/fdk-aac-thin-output/arm64"
 CC="xcrun -sdk iphoneos clang" \
 AS="gas-preprocessor.pl $CC"
-CFLAGS="-arch arm64 -mios-simulator-version-min=7.0" \
-LDFLAGS="-arch arm64 -mios-simulator-version-min=7.0"
+CFLAGS="-arch arm64 -miphoneos-version-min=7.0" \
+LDFLAGS="-arch arm64 -miphoneos-version-min=7.0"
 make clean
 make -j8
 make install
